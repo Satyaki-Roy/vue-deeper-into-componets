0401e8880489346619aa2492a8cbf28f9e008734
Satyaki Roy <satyaki.roy@morningstar.com>
2020-11-06 20:21:31 +0530

    advanced slot concepts

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,12 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goal>
+      <template #default="slotProps">
+        <h2>{{slotProps.item}}</h2>
+        <p>{{slotProps.spacing}}</p>
+      </template>
+    </course-goal>
   </div>
 </template>
 
@@ -14,12 +20,14 @@
 import TheHeader from './components/TheHeader.vue';
 import BadgeList from './components/BadgeList.vue';
 import UserInfo from './components/UserInfo.vue';
+import CourseGoal from "@/components/CourseGoal";
 
 export default {
   components: {
     TheHeader,
     BadgeList,
-    UserInfo
+    UserInfo,
+    CourseGoal
   },
   data() {
     return {
---
+++ src/components/CourseGoal.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul>
+    <li v-for="goal in goals" :key="goal">
+      <slot :item="goal" :spacing="'...'"></slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "CourseGoal",
+  data() {
+    return {
+      goals: [
+        "Component",
+        "vuex",
+      ],
+    }
+  },
+}
+</script>
